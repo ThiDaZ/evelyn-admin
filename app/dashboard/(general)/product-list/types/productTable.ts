016723d497b2a1c8f375e830637eeb9dd9f22f25
ThiDaZ <wthidas95@gmail.com>
2025-02-14 12:40:23 +0530

    type bugs on product

--- app/dashboard/(general)/product-list/types/productTable.ts
+++ app/dashboard/(general)/product-list/types/productTable.ts
@@ -1,4 +1,4 @@
 import type { Product } from "./product"
 
-export type ProductTableData = Pick<Product, "id" | "name" | "price" | "stock" | "rating"> & { image: string }
+export type ProductTableData = Pick<Product, "productID" | "name" | "price" | "stock" | "rating"> & { image: string }
 
